29e9aa7a0d7463a938058187383e1794212d6d54
MigueSamaniego <celihen92@gmail.com>
2025-11-18 15:22:46 +0100

    Escenario with wifi and cellular interfaces ready

---
+++ inet4.4/src/inet/networklayer/configurator/ipv4/HostAutoConfiguratorAP_M.cc
@@ -0,0 +1,148 @@
+//
+// Copyright (C) 2009 Christoph Sommer <christoph.sommer@informatik.uni-erlangen.de>
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+
+#include "inet/networklayer/configurator/ipv4/HostAutoConfiguratorAP_M.h"
+//src.node_bus.configurator.ipv4
+#include <algorithm>
+
+#include "inet/common/ModuleAccess.h"
+#include "inet/common/lifecycle/ModuleOperations.h"
+#include "inet/common/lifecycle/NodeStatus.h"
+#include "inet/networklayer/common/L3AddressResolver.h"
+#include "inet/networklayer/contract/IInterfaceTable.h"
+#include "inet/networklayer/contract/ipv4/Ipv4Address.h"
+#include "inet/networklayer/ipv4/Ipv4InterfaceData.h"
+
+namespace inet {
+
+Define_Module(HostAutoConfiguratorAP_M);
+
+void HostAutoConfiguratorAP_M::initialize(int stage)
+{
+    OperationalBase::initialize(stage);
+    if (stage == INITSTAGE_LOCAL) {
+        interfaceTable.reference(this, "interfaceTableModule", true);
+    }
+}
+
+void HostAutoConfiguratorAP_M::finish()
+{
+}
+
+void HostAutoConfiguratorAP_M::handleMessageWhenUp(cMessage *apMsg)
+{
+}
+
+void HostAutoConfiguratorAP_M::setupNetworkLayer()
+{
+    EV_INFO << "host auto configuration started" << std::endl;
+
+    std::string interfaces = par("interfaces");
+    Ipv4Address addressBase = Ipv4Address(par("addressBase").stringValue());
+    Ipv4Address netmask = Ipv4Address(par("netmask").stringValue());
+
+    // Interface IPV4
+    Ipv4Address addressBasePPP = Ipv4Address(par("addressBasePPP").stringValue());
+    Ipv4Address netmaskPPP = Ipv4Address(par("netmaskPPP").stringValue());
+
+    std::string mcastGroups = par("mcastGroups").stdstringValue();
+    EV_INFO << "INTERFACES: " << interfaces << std::endl;
+    EV_INFO << "addresbase: " << addressBase << std::endl;
+    EV_INFO << "netmask: " << netmask << std::endl;
+
+    // get our host module
+    cModule *host = getContainingNode(this);
+    EV_INFO << "host ID : " << host->getId() << std::endl;
+    Ipv4Address myAddress = Ipv4Address(addressBase.getInt());
+    Ipv4Address myAddressPPP = Ipv4Address(addressBasePPP.getInt() + uint32_t(host->getId()));
+    EV_INFO << "myAddress: " << myAddress << std::endl;
+    // address test
+    if (!Ipv4Address::maskedAddrAreEqual(myAddress, addressBase, netmask))
+        throw cRuntimeError("Generated IP address is out of specified address range");
+
+    //address test
+    if (!Ipv4Address::maskedAddrAreEqual(myAddressPPP, addressBasePPP, netmaskPPP))
+        throw cRuntimeError("Generated IP address PPP is out of specified address range");
+
+    // get our routing table
+    IIpv4RoutingTable *routingTable = L3AddressResolver().getIpv4RoutingTableOf(host);
+
+    if (!routingTable)
+        throw cRuntimeError("No routing table found");
+
+    // look at all interface table entries
+    cStringTokenizer interfaceTokenizer(interfaces.c_str());
+    const char *ifname;
+    uint32_t loopbackAddr = Ipv4Address::LOOPBACK_ADDRESS.getInt();
+
+    while ((ifname = interfaceTokenizer.nextToken()) != nullptr) {
+        NetworkInterface *ie = interfaceTable->findInterfaceByName(ifname);
+        EV_INFO << "val ie : " << ie << std::endl;
+        if (!ie)
+            throw cRuntimeError("No such interface '%s'", ifname);
+
+        auto ipv4Data = ie->getProtocolDataForUpdate<Ipv4InterfaceData>();
+        EV_INFO << "val ipv4Data : " << ipv4Data << std::endl;
+        // assign IP Address to all connected interfaces
+        if (ie->isLoopback()) {
+            ipv4Data->setIPAddress(Ipv4Address(loopbackAddr++));
+            ipv4Data->setNetmask(Ipv4Address::LOOPBACK_NETMASK);
+            ipv4Data->setMetric(1);
+            EV_INFO << "loopback interface " << ifname << " gets " << ipv4Data->getIPAddress() << "/" << ipv4Data->getNetmask() << std::endl;
+            continue;
+        }
+        EV_INFO << "nombre interface usada : " << ifname << std::endl;
+        if (!strcmp(ifname, "wlan0")) {
+            myAddress = Ipv4Address(addressBase.getInt());
+            EV_INFO << "interface cellular : " << myAddress << std::endl;
+        }else if (!strcmp(ifname, "ppp0")) {
+            myAddress = Ipv4Address(addressBasePPP.getInt() + uint32_t(host->getId()));
+            netmask = netmaskPPP;
+            EV_INFO << "interface PPP OK : " << myAddress << std::endl;
+        }
+
+        EV_INFO << "interface " << ifname << " gets " << myAddress.str() << "/" << netmask.str() << std::endl;
+
+        ipv4Data->setIPAddress(myAddress);
+        ipv4Data->setNetmask(netmask);
+        ie->setBroadcast(true);
+
+        // associate interface with default multicast groups
+        ipv4Data->joinMulticastGroup(Ipv4Address::ALL_HOSTS_MCAST);
+        ipv4Data->joinMulticastGroup(Ipv4Address::ALL_ROUTERS_MCAST);
+
+        // associate interface with specified multicast groups
+        cStringTokenizer interfaceTokenizer(mcastGroups.c_str());
+        const char *mcastGroup_s;
+        while ((mcastGroup_s = interfaceTokenizer.nextToken()) != nullptr) {
+            Ipv4Address mcastGroup(mcastGroup_s);
+            ipv4Data->joinMulticastGroup(mcastGroup);
+        }
+
+    }
+}
+
+void HostAutoConfiguratorAP_M::handleStartOperation(LifecycleOperation *operation)
+{
+    if (operation == nullptr) {
+        // in initialize:
+        for (int i = 0; i < interfaceTable->getNumInterfaces(); i++)
+            interfaceTable->getInterface(i)->addProtocolData<Ipv4InterfaceData>();
+    }
+    setupNetworkLayer();
+}
+
+void HostAutoConfiguratorAP_M::handleStopOperation(LifecycleOperation *operation)
+{
+}
+
+void HostAutoConfiguratorAP_M::handleCrashOperation(LifecycleOperation *operation)
+{
+}
+
+} // namespace inet
+
